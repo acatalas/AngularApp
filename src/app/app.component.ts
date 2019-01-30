cc26d33803788b5f3d1afdfe8aa945915d27f92e
acatalas <acatala@iesfbmoll.org>
2019-01-30 20:19:27 +0100

    Data binding part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+/*export class AppComponent {
   title = 'My first angular app';
+}*/
+export class AppComponent {
+  //1- Event binding: How to pass from the TypeScrypt (the class) into the view (html)
+  onClickMe(val){
+    alert(val);
+  }
+  //2- Event Binding: How to pass from the typescrypt (the class) into the view (html)
+  clickMessage = '';
+  clicks = 1;
+  onClickMe2(event: any){
+    let evtMsg = event ? 'Event target is ' + event.target.tagName : '';
+    this.clickMessage = (`Click #${this.clicks++}. ${evtMsg} `);
+  }
 }
